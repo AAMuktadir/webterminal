7a671f3d7a55aa47069919d1d5083df4aca8ae45
AAMuktadir <md.abdullah.al.muktadir@g.bracu.ac.bd>
2025-01-30 14:44:28 +0600

    contact

--- components/directories/about/contact.jsx
+++ components/directories/about/contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Contact() {
   return (
     <div>
-      Phone: 01734007734 <br />
+      Phone: +880 1734007734 <br />
       Email: muktadir.96@gmail.com
     </div>
   );
